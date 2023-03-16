83850b8d99884113a69a69b8f519d9e5e1386ebf
bonheurdev <irbonheur3@gmail.com>
2023-03-16 16:04:30 -0700

    a C program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19, followed by a new line, to the standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ *
+ * Description:'this progam will print text without using printf or puts'
+ *
+ * Return: 1(success)
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
