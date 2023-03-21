678d47e22c986d00a8380b8acf922b248cec6631
bonheurdev <irbonheur3@gmail.com>
2023-03-21 01:21:22 -0700

    a program that prints _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * _putchar: print the alphabets using main.h
+ *
+ * @parameters: not yet
+ *
+ * Return: success if 0
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
+
