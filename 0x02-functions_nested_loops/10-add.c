517291f61532382ec1c18433d13a117db67ac78f
bonheurdev <irbonheur3@gmail.com>
2023-03-21 07:56:11 -0700

    function that adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+# include "main.h"
+/**
+* add - function that prints all alphabets 10 times
+*
+* @i: parameter takes in character
+* @j: parameter takes in character
+*Return: Always 0.
+*/
+int add(int i, int j)
+{
+return (i + j);
+}
