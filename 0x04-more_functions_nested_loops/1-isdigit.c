170ce909b4faf0cdc6e071e5ec82cb6e5f93b216
bonheurdev <irbonheur3@gmail.com>
2023-03-23 04:47:18 -0700

    checking for digits

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - Entry point
+ *@i: takes in digit
+ *Return: 1 for uppercase 0 otherwise
+ */
+int _isdigit(int i)
+{
+if (i >= 48 && i <= 57)
+{
+return (1);
+}
+return (0);
+}
